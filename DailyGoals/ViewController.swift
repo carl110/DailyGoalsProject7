a8137af141b26d2ef8778b3509fc5524a107e580
Carl Wainwright <carljame7@gmail.com>
2019-01-14 08:58:59 +0000

    Initial Commit

---
+++ DailyGoals/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DailyGoals
+//
+//  Created by Carl Wainwright on 14/01/2019.
+//  Copyright © 2019 Carl Wainwright. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
