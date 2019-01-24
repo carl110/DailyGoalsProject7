120ba29b96e6fcc0f7501c6344603997afabbbac
Carl Wainwright <carljame7@gmail.com>
2019-01-24 12:56:54 +0000

    put date extension into its own file

---
+++ DailyGoals/Extensions/Date+extensions.swift
@@ -0,0 +1,18 @@
+//
+//  Date+extensions.swift
+//  DailyGoals
+//
+//  Created by Carl Wainwright on 24/01/2019.
+//  Copyright © 2019 Carl Wainwright. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension Date {
+    func string(format: String) -> String {
+        let formatter = DateFormatter()
+        formatter.dateFormat = format
+        return formatter.string(from: self)
+    }
+}
