7201ff7046172947ebaeb9f2a1a627394c5d848a
Carl Wainwright <carljame7@gmail.com>
2019-02-27 13:24:20 +0000

    updated test

--- DailyGoalsTests/DailyGoalsTests.swift
+++ DailyGoalsTests/DailyGoalsTests.swift
@@ -19,29 +19,30 @@ class DailyGoalsTests: XCTestCase {
     }
     
     func testIfDataPullsThroughToASrraysFromAlertBox() {
-        dailyTaskVC.showGoalTaskDialog(title: "Title",
-                                       subtitle: "Subtitle",
-                                       actionTitle: "ActionTitle",
-                                       goalPlaceHolder: "Goal",
-                                       task1PlaceHolder: "Task 1",
-                                       task2PlaceHolder: "Task 2",
-                                       task3PlaceHolder: "Task 3",
+        
+        let goalText = "Goal"
+        let task1Test = "Task1"
+        let task2Test = "Task 2"
+        let task3Test = "Task 3"
+        
+        
+        dailyTaskVC.showGoalTaskDialog(goalPlaceHolder: goalText,
+                                       task1PlaceHolder: task1Test,
+                                       task2PlaceHolder: task2Test,
+                                       task3PlaceHolder: task3Test,
                                        actionHandler: { (goalInput:String?, task1Input:String?, task2Input:String?, task3Input:String?) in
+                                        self.dailyTaskVC.sectionData = [DailyGoalData(text: "\(Date().string(format: "dd MMM yyyy")) \n \(goalInput ?? "")")]
+                                        self.dailyTaskVC.cellsData = [CellData(text: "\(task1Input ?? "")" ),
+                                                                      CellData(text: "\(task2Input ?? "")" ),
+                                                                      CellData(text: "\(task3Input ?? "")" )]
                                         
-                                        if (goalInput?.isEmpty)! || (task1Input?.isEmpty)! || (task2Input?.isEmpty)! || (task3Input?.isEmpty)! {
-                                            
-                                            self.dailyTaskVC.warningMessage()
-                                            
-                                        } else {
-                                            self.dailyTaskVC.sectionData = [DailyGoalData(text: "\(Date().string(format: "dd MMM yyyy")) \n \(goalInput ?? "")")]
-                                            self.dailyTaskVC.cellsData = [CellData(text: "\(task1Input ?? "")" ),
-                                                              CellData(text: "\(task2Input ?? "")" ),
-                                                              CellData(text: "\(task3Input ?? "")" )]
-                                            self.dailyTaskVC.dailyTaskTableView.reloadData()
-                                        }
+                                        
         })
         XCTAssertEqual(dailyTaskVC.cellsData.count, 3)
-        XCTAssertEqual(dailyTaskVC.sectionData[0].text, "Title")
+        XCTAssertEqual(dailyTaskVC.sectionData[0].text, goalText)
+        XCTAssertEqual(dailyTaskVC.cellsData[0].text, task1Test)
+        XCTAssertEqual(dailyTaskVC.cellsData[1].text, task2Test)
+        XCTAssertEqual(dailyTaskVC.cellsData[2].text, task3Test)
         
     }
 }
